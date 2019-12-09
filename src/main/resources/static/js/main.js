2d8c7c8b1f7826361919f243374498a63e4083da
Dmitrenko <Dias64@mail.ru>
2019-12-09 09:53:06 +0300

    Comment

--- src/main/resources/static/js/main.js
+++ src/main/resources/static/js/main.js
@@ -1,29 +1,30 @@
 $(document).ready(function () {
-    $("#btnPost").click(function (e) {
+    $("#btnPost").click(function (e) { // При нажатии на кнопку с id=btnPost
         e.preventDefault();
-        $.ajax({
+        $.ajax({                       // Выполняем ajax-запрос
             type: "POST",
-            url: "/app/ajax_demo",
-            data: {
+            url: "/app/ajax_demo",     // URL запроса
+            data: {                    // Данные, которые хочем добавить в запрос
                 index: 1,
                 name: "bob"
             },
-            success: function (result) {
-                alert("Прибыли данные: " + result);
-                updateCounter();
+            success: function (result) {                 // Если все OK
+                alert("Прибыли данные: " + result);      // Показываем всплывающее окно
+                updateCounter();                         // Вызываем функцию
             },
-            error: function (msg) {
+            error: function (msg) {                      // Если ошибка, показываем alert с сообщением
                 alert("Oops");
             }
         });
     });
 
-    function updateCounter() {
+    function updateCounter() {               // Обновляем счетчик товаров через ajax
         $.ajax({
-            type: "POST",
-            url: "/app/cart_count_request",
-            success: function (result) {
-                console.log(result);
+            type: "POST",                    // шлем POST
+            url: "/app/cart_count_request",  // на этот адрес
+            success: function (result) {     // если пришел ответ
+                console.log(result);         // печатаем его в консоль
+                // в метку с id=lblCounter пишем текст с пришедшим из BackEnd-a результатом
                 $('#lblCounter').text('Товаров в корзине: ' + result);
             }
         });
